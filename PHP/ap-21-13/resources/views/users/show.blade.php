0bd50bb7d6f682c2e11bea8a494f27360fa01622
levantrinh <levantrinhpro@gmail.com>
2019-11-13 16:24:07 +0700

    tich hop CkEditor and CKFinder

--- PHP/ap-21-13/resources/views/users/show.blade.php
+++ PHP/ap-21-13/resources/views/users/show.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Show 1 người dùng</title>
+    <title>Xem 1 người dùng</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.css">
 </head>
 <body>
